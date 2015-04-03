9a2405f910e1397c9004cbe6febeda0cf9530e02
Ryan Dowling <rldowling03@gmail.com>
2015-04-04 02:04:08 +1100

    Add in time command

---
+++ commands/basicCommands/timeCommand.js
@@ -0,0 +1,36 @@
+/*
+ * AllmightyBot - https://github.com/RyanTheAllmighty/AllmightyBot
+ * Copyright (C) 2015 RyanTheAllmighty
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+var connection = require('../../inc/connection');
+
+var lang = require('../../lang.json');
+var settings = require('../../settings.json');
+
+module.exports.enabled = true;
+
+module.exports.name = 'time';
+
+module.exports.callback = function (command_name, channel, user, message) {
+    var currentdate = new Date();
+
+    var datetime = ((currentdate.getHours() < 10) ? "0" : "") + ((currentdate.getHours() > 12) ? (currentdate.getHours() - 12) : currentdate.getHours()) + ":"
+        + ((currentdate.getMinutes() < 10) ? "0" : "") + currentdate.getMinutes() + ":" + ((currentdate.getSeconds() < 10) ? "0" : "") + currentdate.getSeconds()
+        + ((currentdate.getHours() > 12) ? (' PM') : ' AM');
+
+    connection.client.sendMessage(channel, lang.current_time.format(settings.casters_display_name, datetime));
+};
